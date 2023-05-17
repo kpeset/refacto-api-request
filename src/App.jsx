0603ef1ad7e715e428a4aee97bf0daae06f17f3c
kpeset <kevin.peset@gmail.com>
2023-05-17 13:23:07 +0200

    Ajout message de chargement

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,30 @@ const App = () => {
   const simpsons_api = "https://thesimpsonsquoteapi.glitch.me/quotes";
   const simpsonsInfo = useFetchApi(simpsons_api);
 
+  const displayCard = () => {
+    return simpsonsInfo.map((infos, index) => (
+      <Card
+        key={index}
+        character={infos.character}
+        quote={infos.quote}
+        image={infos.image}
+      />
+    ));
+  };
+
+  const loadingMessage = () => {
+    return (
+      <div>
+        <p>Chargement en cours....</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      {simpsonsInfo.map((infos, index) => (
-        <Card
-          key={index}
-          character={infos.character}
-          quote={infos.quote}
-          image={infos.image}
-        />
-      ))}
-    </div>
+    <>
+    <h1>Les simpsons</h1>
+      {simpsonsInfo.length === 0 ? loadingMessage() : displayCard()}
+    </>
   );
 };
 
